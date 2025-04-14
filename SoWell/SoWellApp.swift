fe585907ec7bc5e0734a6cd2e9b2493d79c805b9
Mook <mpmookr@gmail.com>
2025-04-14 13:39:48 +0100

    Initial Commit

---
+++ SoWell/SoWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoWellApp.swift
+//  SoWell
+//
+//  Created by Mook Rattana on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SoWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
